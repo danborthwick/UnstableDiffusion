efc6c913b8d45ff867d4d4a40ea8d649f115b9c2
Dan Borthwick <dan@smartcasual.cc>
2025-08-15 14:01:20 +1000

    image grid layout

--- game/src/LeftPanel.tsx
+++ game/src/LeftPanel.tsx
@@ -9,15 +9,19 @@ const LeftPanel: React.FC = () => {
       <div className="image-grid">
         <div className="image-grid-item">
           <img src="data/option1.webp" alt="Grid 1" />
+          <div className="image-label">A</div>
         </div>
         <div className="image-grid-item">
           <img src="/data/option1.webp" alt="Grid 2" />
+          <div className="image-label">B</div>
         </div>
         <div className="image-grid-item">
           <img src="/data/option1.webp" alt="Grid 3" />
+          <div className="image-label">C</div>
         </div>
         <div className="image-grid-item">
           <img src="/data/option1.webp" alt="Grid 4" />
+          <div className="image-label">D</div>
         </div>
       </div>
     </div>
